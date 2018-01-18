737edcef91d8a34485278210b4ab8a5c0a0f50b7
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-18 14:51:52 +0000

    Add NavLink to articles titles

--- nc-news/src/components/Article.js
+++ nc-news/src/components/Article.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+
 
 import Votes from './Votes';
 
@@ -8,7 +10,9 @@ const Article = ({ article }) => (
       <Votes votesNum={article.votes} />
     </div>
     <div className="column">
-      <h1>{article.title}</h1>
+      <h1>
+        <NavLink to={`/articles/${article._id}`}>{article.title}</NavLink>
+      </h1>
       <p>added by {article.created_by} to {article.belongs_to}</p>
       <p>{article.comments} comments</p>
     </div>
