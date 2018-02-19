445e24540c89033659c5669e924651399b328174
dzewelina <ewelina.zukowska26@gmail.com>
2018-02-19 18:03:52 +0000

    Remove link from the title of the article

--- src/components/ArticleInfo.js
+++ src/components/ArticleInfo.js
@@ -18,7 +18,7 @@ const ArticleInfo = ({ article, voting, showComments, navLink }) => {
 
       <div className="column">
         <h1 className="articleTitle">
-          <SafeLink to={`/articles/${article._id}?comments=false`}>{article.title}</SafeLink>
+          {navLink ? <SafeLink to={`/articles/${article._id}?comments=false`}>{article.title}</SafeLink> : article.title}
         </h1>
 
         {navLink ? <SafeLink to={`/articles/${article._id}?comments=true`}>{article.comments} comments</SafeLink> : <p onClick={showComments}>{article.comments} comments</p>}
